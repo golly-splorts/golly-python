3d96c1eae8a58182cf27e8dd66fb8f26e3a9bfb6
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-12-13 23:10:22 -0800

    update env var name

--- common.mk
+++ common.mk
@@ -1,10 +1,5 @@
 SHELL=/bin/bash
 
-ifeq ($(shell echo ${GOLLY_API_HOME}),)
-$(error Environment variable GOLLY_API_HOME not defined. Please run "source environment" in the golly-api repo root directory before running make commands)
+ifeq ($(shell echo ${GOLLY_PYTHON_HOME}),)
+$(error Environment variable GOLLY_PYTHON_HOME not defined. Please run "source environment" in the golly-python repo root directory before running make commands)
 endif
-
-#ifeq ($(shell which jq),)
-#$(error Please install jq using "apt-get install jq" or "brew install jq")
-#endif
-
